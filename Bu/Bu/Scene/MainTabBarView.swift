b8256d504e2bb283ee5ca92c0fd1a8eedd2f056e
Giga Khizanishvili <giga.khizanishvili@space.ge>
2024-01-25 09:54:06 +0400

    Create MainTabBar

---
+++ Bu/Bu/Scene/MainTabBarView.swift
@@ -0,0 +1,39 @@
+//
+//  MainTabBarView.swift
+//  Bu
+//
+//  Created by Giga Khizanishvili on 25.01.24.
+//
+
+import SwiftUI
+
+struct MainTabBarView: View {
+    @State private var navigationPath: [NavigationItem] = []
+    var body: some View {
+        NavigationStack(path: $navigationPath) {
+            TabView {
+                Text("First Page")
+                    .tabItem {
+                        Image(systemName: "1.square.fill")
+                        Text("First")
+                    }
+
+                Text("Second Page")
+                    .tabItem {
+                        Image(systemName: "2.square.fill")
+                        Text("Second")
+                    }
+
+                Text("Third Page")
+                    .tabItem {
+                        Image(systemName: "3.square.fill")
+                        Text("Third")
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    MainTabBarView()
+}
---
+++ Bu/Bu/Scene/Navigation/NavigationItem.swift
@@ -0,0 +1,10 @@
+//
+//  NavigationItem.swift
+//  Bu
+//
+//  Created by Giga Khizanishvili on 25.01.24.
+//
+
+enum NavigationItem {
+    case settings
+}
